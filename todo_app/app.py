f3ac46d98fc17d9c10dd67695fe1d147a2fa4619
Feargus Brickley <github@fbrickley.fmail.co.uk>
2021-02-17 20:43:42 +0000

    users can now submit new todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask.templating import render_template
-from .data.session_items import get_items
+from .data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -10,8 +10,18 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
+    return load_index()
+
+
+def load_index():
     items = get_items()
     return render_template('index.html', items=items)
+
+
+@app.route("/todos/submit", methods=['POST'])
+def add_todo():
+    add_item(request.form.get('title'))
+    return load_index()
 
 
 if __name__ == '__main__':
