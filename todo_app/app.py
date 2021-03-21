a0908828a5757849958c4529f694fac0acaba05f
Feargus Brickley <github@fbrickley.fmail.co.uk>
2021-03-21 16:54:56 +0000

    users can now search for boards

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, redirect
 from flask.templating import render_template
 from .data.session_items import get_items, add_item
+import requests
+import os
+import json
 
 from todo_app.flask_config import Config
 
@@ -10,8 +13,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    items = get_items()
-    return render_template('index.html', items=items)
+    return render_template('index.html')
 
 
 @app.route("/todos/submit", methods=['POST'])
@@ -22,3 +24,36 @@ def add_todo():
 
 if __name__ == '__main__':
     app.run()
+
+BASE_URL = "https://api.trello.com"
+TRELLO_API_KEY = os.getenv('TRELLO_API_KEY')
+TRELLO_TOKEN = os.getenv('TRELLO_TOKEN')
+auth_header = {
+    "Authorization": f'OAuth oauth_consumer_key="{TRELLO_API_KEY}", oauth_token="{TRELLO_TOKEN}"'}
+
+# show homepage with search for board title
+# results will be a list of matching boards, with a link to /board/id
+# /board/id will show our todos
+
+# @app.route("/board/<id>")
+# def show_board(id):
+
+
+@app.route('/search/boards', methods=['POST'])
+def search_boards():
+    url = BASE_URL + "/1/search"
+    query = request.form.get('title')
+    payload = {"query": query, "idBoards": "mine",
+               "modelTypes": "boards", "partial": "true"}
+    response = requests.get(url, params=payload, headers=auth_header)
+    if response.ok:
+        try:
+            boards = response.json()["boards"]
+            return render_template('boards.html', boards=boards)
+        except:
+            return redirect("/")
+
+    # pparse boards for search results
+    # if exaclty one then redirect to board page
+    # if more than one then redirect to board list page
+    # if zero then redirect to search results (somehow show message saying nothing found)
